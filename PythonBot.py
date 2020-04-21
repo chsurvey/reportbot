319f1af2b614511a7f6281762b38d97fa24fd670
chsurvey <58624692+chsurvey@users.noreply.github.com>
2020-04-21 13:59:26 +0900

    Update PythonBot.py

--- PythonBot.py
+++ PythonBot.py
@@ -22,6 +22,9 @@ async def on_message(message): # 메시지가 들어 올 때마다 가동되는 
     if message.attachments and (message.channel.type is discord.ChannelType.private):
         await message.channel.send("이미지는 보낼 수 없습니다")  
         return
+    if 'http' in message.content: and (message.channel.type is discord.ChannelType.private):
+        await message.channel.send("는 보낼 수 없습니다")  
+        return
     if  (message.channel.type is discord.ChannelType.private): #and message.author.id!="700608102269059083":#제 봇 아이디
         #channel = client.get_channel(558908366739734530)
         #await channel.send(message.author.name+" : "+message.content)#관리자에게 메세지가 가는 방식.
