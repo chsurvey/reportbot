7d43fba22c8210f538e86bd8c83f0eeeef23742b
chsurvey <58624692+chsurvey@users.noreply.github.com>
2020-04-26 03:36:14 +0900

    Update PythonBot.py

--- PythonBot.py
+++ PythonBot.py
@@ -84,8 +84,7 @@ async def on_message(message): # 메시지가 들어 올 때마다 가동되는 
                         await channel.send(message.attachments[i].url)#관리자에게 메세지가 가는 방식.
                     return
     if message.content.startswith('!답장'):
-        user = client.get_user(int(message.content[4:22]))
-        channel = await create_dm(user)
+        channel = user.dm_channel
         await channel.send("답변 : "+str(message.content[23:]))     
 access_token=os.environ["BOT_TOKEN"]
 client.run(access_token) # 아까 넣어놓은 토큰 가져다가 봇을 실행하라는 부분입니다. 이 코드 없으면 구문이 아무리 완벽해도 실행되지 않습니다.
